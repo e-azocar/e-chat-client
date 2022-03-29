958b6de85477facd2d341dac76eb72832364f940
Emmanuel Azócar <azocarmel@gmail.com>
2022-03-29 21:57:38 +0000

    connected to socket.io server

--- src/components/Chat/Header.js
+++ src/components/Chat/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
       </BackButton>
       <HeaderImage src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" />
       <HeaderTextContainer>
-        <HeaderTitle>Emmanuel Azócar</HeaderTitle>
+        <HeaderTitle>User Name</HeaderTitle>
         <HeaderSubtitle>En línea</HeaderSubtitle>
       </HeaderTextContainer>
     </HeaderContainer>
--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { FiSend } from "react-icons/fi";
 import {
   ChatContainer,
@@ -10,25 +10,43 @@ import {
 } from "../components/Chat/Chat.styled";
 import Header from "../components/Chat/Header";
 import Message from "../components/Chat/Message";
+import { io } from "socket.io-client";
+import { SERVER_URL } from "../config";
 
-const messages = [
-  "Hello World",
-  "Hola Mundo",
-];
+const socket = io(SERVER_URL);
 
 const Chat = () => {
+  const [userMessage, setUserMessage] = useState("");
+  const [messages, setMessages] = useState([{ text: "Hello" }]);
+
+  socket.on("message:new", (data) => {
+    setMessages([...messages, data]);
+  });
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socket.emit("message:new", { text: userMessage, user: "admin" });
+    setUserMessage("");
+  };
+
+  const changeHandler = (e) => setUserMessage(e.target.value);
+
   return (
     <ChatContainer>
       <Header />
       <ChatMessagesContainer>
         {messages.map((msg, i) => (
-          <Message text={msg} key={i} toMe={i % 2 === 0} />
+          <Message text={msg.text} key={i} toMe={i % 2 === 0} />
         ))}
       </ChatMessagesContainer>
 
       <ChatFooter>
-        <ChatForm>
-          <MessageInput placeholder="Escribe un mensaje aquí" />
+        <ChatForm onSubmit={sendMessage}>
+          <MessageInput
+            placeholder="Escribe un mensaje aquí"
+            onChange={changeHandler}
+            value={userMessage}
+          />
           <SendButton>
             <FiSend />
           </SendButton>
